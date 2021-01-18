0e68ee7c25a45f1b153cb255a0b08043e79f8292
TransientGames <77314438+TransientGames@users.noreply.github.com>
2021-01-17 19:43:56 -0500

    Moved Scripts
    Moved scripts to be in the scripts file instead of under myassets/scripts

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int playerlvl = 1;
+    public int strengthlvl = 1;
+    public int magiclvl = 1;
+    public int stealthlvl = 1;
+    public int lockpicklvl = 1;
+
+
+    public float health = 100f;
+    public float stamina = 100f;
+    public float mana = 100f;
+
+}
